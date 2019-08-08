0bbc149e820900db18da0354c43b0901fe737245
vitaliyRysich <vitaliy.rysich@gmail.com>
2019-08-08 21:22:34 +0200

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+create table person
+(
+  id integer not null,
+  name varchar (255) not null,
+  location varchar (255),
+  birth_date timestamp,
+  primary key (id)
+);
+
+insert into person
+  (id, name, location, birth_date)
+values
+    (10001, 'Vitalii', 'Wroclaw', sysdate()),
+    (10002, 'Sweta', 'Wroclaw', sysdate()),
+    (10003, 'Pasza', 'Berdychiv', sysdate());
